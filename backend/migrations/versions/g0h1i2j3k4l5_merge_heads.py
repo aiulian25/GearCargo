c64f6aefad20e7346e78f8bcbb72915379c3626c
iulian <email@iulian.uk>
2026-04-29 14:09:23 +0100

    Fix: merge Alembic heads to resolve multiple head revisions error

---
+++ backend/migrations/versions/g0h1i2j3k4l5_merge_heads.py
@@ -0,0 +1,23 @@
+"""merge heads
+
+Revision ID: g0h1i2j3k4l5
+Revises: a3c7e2f5b091, e3f4a5b6c789
+Create Date: 2026-04-29 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'g0h1i2j3k4l5'
+down_revision = ('a3c7e2f5b091', 'e3f4a5b6c789')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
